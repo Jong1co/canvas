8d00fc97198886a5264982a5943f5f4152a5ed8b
Jong1co <narira0922@gmail.com>
2024-01-03 17:19:44 +0900

    이해안감 2

--- src/utils/wave.ts
+++ src/utils/wave.ts
@@ -49,13 +49,28 @@ export class WaveImpl implements Wave {
     let prevY = this.points[0].pointY;
     ctx.moveTo(prevX, prevY);
     //TODO: 여기 이해 안 감
-    for (let i = 0; i < this.numberOfPoints; i++) {
-      ctx.lineTo(this.points[i].pointX, this.points[i].pointY);
+    for (let i = 1; i < this.numberOfPoints; i++) {
+      // ctx.lineTo(this.points[i].pointX, this.points[i].pointY);
+
+      const cx = (prevX + this.points[i].pointX) / 2;
+      const cy = (prevY + this.points[i].pointY) / 2;
+
+      ctx.quadraticCurveTo(prevX, prevY, cx, cy); //곡선 그리는 것 //TODO: 어떻게 그리는지?
+
+      /* 곡선을 그리기 위해 이전 좌표 업데이트하기 */
+      prevX = this.points[i].pointX;
+      prevY = this.points[i].pointY;
 
       if (i != 0 && i != this.numberOfPoints - 1) {
         this.points[i].draw(ctx);
       }
     }
+    /* 붓을 오른쪽 모서리부터 왼쪽 모서리 그리고 첫번째 점 위치까지 옮기면서 색칠해줍니다. */
+    ctx.lineTo(prevX, prevY);
+    ctx.lineTo(this.stageWidth, this.stageHeight);
+    ctx.lineTo(0, this.stageHeight);
+    ctx.lineTo(this.points[0].pointX, this.points[0].pointY);
+
     /* 색상 RED & 채우기 */
     ctx.fillStyle = "#ff0000";
     ctx.fill();
